fe1d7bbb59c8158cc6bcb100bfe5350a5ccf2afa
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-03 11:41:41 -0300

    Crear esquemas y Modelos

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
+import { MongooseModule } from '@nestjs/mongoose/dist';
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
