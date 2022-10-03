aa4f21ce4f5c2af03c50856bff1406654dae16a9
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-03 13:32:24 -0300

    Validar y eliminar en una sola consulta

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -76,9 +76,12 @@ export class PokemonService {
     // // const pokemon = await this.findOne(id);
     // // await pokemon.deleteOne();
     // return { id };
-    const result = this.pokemonModel.findByIdAndDelete(id);
+    // const result = await this.pokemonModel.findByIdAndDelete(id);
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
 
-    return result;
+    return;
   }
 
   private handleExceptions(error: any) {
