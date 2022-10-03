04965c7a334e4c38319c5859808f41275ec3e08e
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-03 14:20:55 -0300

    Insertar multiples registros simultáneamente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -16,17 +16,37 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(async ({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
     });
 
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    // insert into pokemons (name, no)
+    // (name: bulbasaur, no: 1)
+
     return 'Seed executed';
+
+    // const insertPromisesArray = [];
+
+    // data.results.forEach(({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+
+    //   // const pokemon = await this.pokemonModel.create({ name, no });
+    //   insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+    // });
+
+    // await Promise.all(insertPromisesArray);
   }
 }
