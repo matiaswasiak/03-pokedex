1bb7734744ae81ee81a8ee937a1610c44b2fab8d
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-03 18:22:56 -0300

    Configuración de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
